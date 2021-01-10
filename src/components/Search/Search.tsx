eb2e769e95b3c672b811346ee582c1c381b8083f
kin-au <kinnamau@gmail.com>
2021-01-10 22:35:33 +0000

    handle different searchtypes

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -9,35 +9,43 @@ interface SearchProps {
 
 function Search(props: SearchProps) {
   const [searchText, setSearchText] = React.useState("");
-  const searchValue = (event: any) => {
-    setSearchText(event.target.value);
-  };
+  const [searchType, setSearchType] = React.useState("user");
 
   return (
     <>
-      <label htmlFor="searchtext">Search</label>
-      <input
-        id="searchtext"
-        type="search"
-        placeholder="Find a GitHub user or organisation"
-        value={searchText}
-        onChange={(event) => searchValue(event)}
-        autoFocus
-        required
-      ></input>
-      <label htmlFor="searchtype">Type</label>
-      <select id="searchtype">
-        <option value="user">User</option>
-        <option value="organisation">Organisation</option>
-      </select>
-      <button
-        onClick={(event) => {
-          event.preventDefault();
-          getUser(searchText).then((data) => console.log(data));
-        }}
-      >
-        Add
-      </button>
+      <form>
+        <label htmlFor="searchtext">Search</label>
+        <input
+          id="searchtext"
+          type="search"
+          placeholder="Find a GitHub user or organisation"
+          value={searchText}
+          onChange={(event) => setSearchText(event.target.value)}
+          autoFocus
+          required
+        ></input>
+        <label htmlFor="searchtype">Type</label>
+        <select
+          id="searchtype"
+          value={searchType}
+          onChange={(event) => setSearchType(event.target.value)}
+        >
+          <option value="user">User</option>
+          <option value="organisation">Organisation</option>
+        </select>
+        <button
+          onClick={(event) => {
+            event.preventDefault();
+            if (searchType === "user") {
+              getUser(searchText).then((data) => console.log(data));
+            } else if (searchType === "organisation") {
+              getOrg(searchText).then((data) => console.log(data));
+            }
+          }}
+        >
+          Add
+        </button>
+      </form>
     </>
   );
 }
