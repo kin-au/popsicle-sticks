bc405d47ef9ed843989eb811f369b500efad9357
kin-au <kinnamau@gmail.com>
2021-01-11 23:01:42 +0000

    fetch with token in headers

--- src/utils/getOrg.tsx
+++ src/utils/getOrg.tsx
@@ -1,13 +1,20 @@
+const token: string = process.env.GITHUB_TOKEN!;
+
 const checkResponse = (response: any) => {
   if (response.status !== 200) {
-    console.log(`Error with the request! ${response.status}`);
+    console.log(`Request error code: ${response.status}`);
     return;
   }
   return response.json();
 };
 
 const getOrg = (organisation: string) => {
-  return fetch(`https://api.github.com/orgs/${organisation}/members`)
+  return fetch(`https://api.github.com/orgs/${organisation}/members`, {
+    method: "GET",
+    headers: {
+      token: token,
+    },
+  })
     .then(checkResponse)
     .catch((err) => {
       throw new Error(`getOrg failed with error: ${err}`);
--- src/utils/getUser.tsx
+++ src/utils/getUser.tsx
@@ -1,13 +1,20 @@
+const token: string = process.env.GITHUB_TOKEN!;
+
 const checkResponse = (response: any) => {
   if (response.status !== 200) {
-    console.log(`Error with the request! ${response.status}`);
+    console.log(`Request error code: ${response.status}`);
     return;
   }
   return response.json();
 };
 
 const getUser = (username: string) => {
-  return fetch(`https://api.github.com/users/${username}`)
+  return fetch(`https://api.github.com/users/${username}`, {
+    method: "GET",
+    headers: {
+      token: token,
+    },
+  })
     .then(checkResponse)
     .catch((err) => {
       throw new Error(`getUser failed with error: ${err}`);
