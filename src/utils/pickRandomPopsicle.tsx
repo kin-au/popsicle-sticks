0158a631b1a36d190927162c5f641841bd28ebbe
kin-au <kinnamau@gmail.com>
2021-01-20 23:46:18 +0000

    pick random user function

---
+++ src/utils/pickRandomPopsicle.tsx
@@ -0,0 +1,17 @@
+interface User {
+  avatar: string;
+  url: string;
+  username: string;
+  id: number;
+  selected: boolean;
+}
+
+interface UserList extends Array<User> {}
+
+const pickRandomPopsicle = (userList: UserList): User => {
+  const user = userList[Math.floor(Math.random() * userList.length)];
+  user.selected = true;
+  return user;
+};
+
+export default pickRandomPopsicle;
