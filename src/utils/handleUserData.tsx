332fe13bce4e2b7d03e81708bc5dece46af53321
kin-au <kinnamau@gmail.com>
2021-01-22 00:07:44 +0000

    handleuserdata handle no data case

--- src/utils/handleUserData.tsx
+++ src/utils/handleUserData.tsx
@@ -12,26 +12,28 @@ type DataType = "user" | "organisation";
 
 const handleUserData = (rawData: any, dataType: DataType): UserList => {
   let data = [];
-  if (dataType === "user") {
-    data = [
-      {
-        avatar: rawData.avatar_url,
-        url: rawData.html_url,
-        username: rawData.login,
-        id: rawData.id,
-        selected: false,
-      },
-    ];
-  } else if (dataType === "organisation") {
-    data = rawData.map((user: any) => {
-      return {
-        avatar: user.avatar_url,
-        url: user.html_url,
-        username: user.login,
-        id: user.id,
-        selected: false,
-      } as User;
-    });
+  if (rawData) {
+    if (dataType === "user") {
+      data = [
+        {
+          avatar: rawData.avatar_url,
+          url: rawData.html_url,
+          username: rawData.login,
+          id: rawData.id,
+          selected: false,
+        },
+      ];
+    } else if (dataType === "organisation") {
+      data = rawData.map((user: any) => {
+        return {
+          avatar: user.avatar_url,
+          url: user.html_url,
+          username: user.login,
+          id: user.id,
+          selected: false,
+        } as User;
+      });
+    }
   }
   return data;
 };
