6f9ff095a81cd95316f7e48e0ccb2cec1ea51a99
kin-au <kinnamau@gmail.com>
2021-01-24 21:30:15 +0000

    add random popsicle feature to ui, and add remove-all-popsicles feature

--- src/components/PopsicleArea/PopsicleArea.tsx
+++ src/components/PopsicleArea/PopsicleArea.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import pickRandomPopsicle from "../../utils/pickRandomPopsicle";
 
 interface User {
   avatar: string;
@@ -15,18 +16,23 @@ interface PopsicleAreaProps {
   // setUser: any;
   userList: UserList;
   setUserList: any;
+  disableElement: boolean;
+  setDisableElement: any;
 }
 
 function PopsicleArea(props: PopsicleAreaProps) {
+  const [selectedUser, setSelectedUser] = React.useState<any>({} as any);
+
   return (
     <>
-      <h2>popsicle area</h2>
-      {props.userList
+      <h2>popsicle pot</h2>
+      {props.userList.length
         ? props.userList.map((user: User) => {
             return (
               <div key={user.id}>
                 <p>{user.username}</p>;
                 <button
+                  disabled={props.disableElement}
                   onClick={() => {
                     let newArr = [...props.userList];
                     const removeIndex = newArr.indexOf(
@@ -42,6 +48,43 @@ function PopsicleArea(props: PopsicleAreaProps) {
             );
           })
         : null}
+      {Object.keys(selectedUser).length === 0 &&
+      selectedUser.constructor === Object ? null : (
+        <div>
+          <h1>{selectedUser.username}</h1>
+          <button
+            onClick={() => {
+              setSelectedUser({});
+              props.setDisableElement(false);
+            }}
+          >
+            OK
+          </button>
+        </div>
+      )}
+      {props.userList.length ? (
+        <button
+          disabled={props.disableElement}
+          onClick={() => {
+            setSelectedUser(pickRandomPopsicle(props.userList));
+            props.setDisableElement(true);
+          }}
+        >
+          Random popsicle
+        </button>
+      ) : null}
+      {props.userList.length ? (
+        <button
+          disabled={props.disableElement}
+          onClick={() => {
+            setSelectedUser({});
+            props.setUserList([]);
+            props.setDisableElement(false);
+          }}
+        >
+          Remove all popsicles
+        </button>
+      ) : null}
     </>
   );
 }
