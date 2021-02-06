0e1a89adee6de1f6ad5255b2fd6540109f9e289e
kin-au <kinnamau@gmail.com>
2021-02-06 21:02:33 +0000

    wip refactor - app file type imports, search function, state changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,59 +2,57 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Search from "./components/Search/Search";
-import PopsicleArea from "./components/PopsicleArea/PopsicleArea";
-import Controls from "./components/Controls/Controls";
+import PopsiclePot from "./components/PopsicleArea/PopsiclePot";
+import Controls from "./components/Controls";
+import getData from "./utils/getData";
+import handleUserData from "./utils/handleUserData";
+import checkDuplicates from "./utils/checkDuplicates";
+import { SearchDataType, UserList } from "./types";
 
-interface User {
-  avatar: string;
-  url: string;
-  username: string;
-  id: number;
-  selected: boolean;
-}
-
-interface UserList extends Array<User> {}
-
-function App() {
-  // const [user, setUser] = React.useState<User | any>({});
-  const [userList, setUserList] = React.useState<UserList | []>([]);
-  const [showingSelected, setShowingSelected] = React.useState<boolean>(false);
-  const [selectedUser, setSelectedUser] = React.useState<any>({} as any);
+const App = () => {
+  const [userList, setUserList] = React.useState<UserList>([]);
+  const [selectedUserId, setSelectedUserId] = React.useState<number | null>(
+    null
+  );
+  // const [previouslyselectedUserId, setpreviouslySelectedUserId] = React.useState<number[]>([]);
   const [rememberSelected, setRememberSelected] = React.useState<boolean>(true);
+
+  const disableInput = selectedUserId != null;
+
+  const searchUsers = async (
+    searchType: SearchDataType,
+    searchText: string
+  ) => {
+    const responseData = await getData(searchType, searchText);
+    const handledData = handleUserData(responseData, searchType);
+    const checkedData = checkDuplicates(userList, handledData);
+    setUserList([...userList, ...checkedData]);
+  };
 
   return (
     <>
       <Header />
-      <Search
+      <Search disableInput={disableInput} searchUsers={searchUsers} />
+      <PopsiclePot
         userList={userList}
         setUserList={setUserList}
-        showingSelected={showingSelected}
-        setShowingSelected={setShowingSelected}
-      />
-      <PopsicleArea
-        // user={user}
-        // setUser={setUser}
-        userList={userList}
-        setUserList={setUserList}
-        selectedUser={selectedUser}
-        setSelectedUser={setSelectedUser}
-        showingSelected={showingSelected}
-        setShowingSelected={setShowingSelected}
+        selectedUserId={selectedUserId}
+        setSelectedUserId={setSelectedUserId}
+        disableInput={disableInput}
         rememberSelected={rememberSelected}
         setRememberSelected={setRememberSelected}
       />
       <Controls
         userList={userList}
         setUserList={setUserList}
-        selectedUser={selectedUser}
-        setSelectedUser={setSelectedUser}
-        showingSelected={showingSelected}
-        setShowingSelected={setShowingSelected}
+        selectedUserId={selectedUserId}
+        setSelectedUserId={setSelectedUserId}
+        disableInput={disableInput}
         rememberSelected={rememberSelected}
         setRememberSelected={setRememberSelected}
       />
     </>
   );
-}
+};
 
 export default App;
