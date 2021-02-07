7cfed0c96c62b5be0546afe567d42f4998a7614b
kin-au <kinnamau@gmail.com>
2021-02-07 00:10:46 +0000

    wip refactor - popsiclestick component

--- src/components/PopsicleArea/PopsicleStick.tsx
+++ src/components/PopsicleArea/PopsicleStick.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 import { PopsicleStickProps } from "./types";
 
-function PopsicleStick(props: PopsicleStickProps) {
+const PopsicleStick = (props: PopsicleStickProps) => {
+  const { user, userList, setUserList, disableInput } = props;
+
   return (
-    <>
-      <p>{props.user.username}</p>
-      {/* <img src={props.user.avatar} alt="avatar" /> */}
-      <button>Remove</button>
-    </>
+    <div key={user.id}>
+      <p>{user.username}</p>
+      <button
+        disabled={disableInput}
+        onClick={() => {
+          let newArr = [...userList];
+          const removeIndex = newArr.indexOf(
+            newArr.find((newUser) => newUser.id === user.id)!
+          );
+          newArr.splice(removeIndex, 1);
+          setUserList(newArr);
+        }}
+      >
+        Remove
+      </button>
+    </div>
   );
-}
+};
 
 export default PopsicleStick;
