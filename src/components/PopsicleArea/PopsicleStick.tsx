ac69cf22f7d06b97a5ecabf11cf27abc116871f8
kin-au <kinnamau@gmail.com>
2021-02-10 00:24:49 +0000

    wip refactor - popsiclestick component

--- src/components/PopsicleArea/PopsicleStick.tsx
+++ src/components/PopsicleArea/PopsicleStick.tsx
@@ -1,8 +1,38 @@
 import React from "react";
+import { User } from "../../types";
 import { PopsicleStickProps } from "./types";
 
 const PopsicleStick = (props: PopsicleStickProps) => {
-  const { user, userList, setUserList, disableInput } = props;
+  const {
+    user,
+    userList,
+    setUserList,
+    previouslySelectedUserId,
+    setPreviouslySelectedUserId,
+    disableInput,
+  } = props;
+
+  const removeFromUserList = () => {
+    let newArr = [...userList];
+    const removeIndex = newArr.indexOf(
+      newArr.find((newUser) => newUser.id === user.id) as User
+    );
+    if (removeIndex !== -1) {
+      newArr.splice(removeIndex, 1);
+      setUserList(newArr);
+    }
+  };
+
+  const removeFromPreviouslySelected = () => {
+    let newArr = [...previouslySelectedUserId];
+    const removeIndex = newArr.indexOf(
+      newArr.find((newId) => newId === user.id) as number
+    );
+    if (removeIndex !== -1) {
+      newArr.splice(removeIndex, 1);
+      setPreviouslySelectedUserId(newArr);
+    }
+  };
 
   return (
     <div key={user.id}>
@@ -10,12 +40,8 @@ const PopsicleStick = (props: PopsicleStickProps) => {
       <button
         disabled={disableInput}
         onClick={() => {
-          let newArr = [...userList];
-          const removeIndex = newArr.indexOf(
-            newArr.find((newUser) => newUser.id === user.id)!
-          );
-          newArr.splice(removeIndex, 1);
-          setUserList(newArr);
+          removeFromUserList();
+          removeFromPreviouslySelected();
         }}
       >
         Remove
