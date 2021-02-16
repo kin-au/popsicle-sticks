0d8baedb0611b1c757308a6ddc71ade979d3d282
kin-au <kinnamau@gmail.com>
2021-02-16 00:09:17 +0000

    wip refactor - define global types

--- src/types.ts
+++ src/types.ts
@@ -9,42 +9,46 @@ export type UserList = User[];
 
 export type SearchDataType = "user" | "organisation";
 
-export type GetDataResponse = UserResponse | OrganisationResponse;
+export type ResponseDataType =
+  | GetUserDataResponse
+  | GetOrganisationDataResponse;
 
-export interface UserResponse {
-  login: string;
-  id: number;
-  node_id: string;
-  avatar_url: string;
-  gravatar_id: string;
-  url: string;
-  html_url: string;
-  followers_url: string;
-  following_url: string;
-  gists_url: string;
-  starred_url: string;
-  subscriptions_url: string;
-  organizations_url: string;
-  repos_url: string;
-  events_url: string;
-  received_events_url: string;
-  type: string;
-  site_admin: boolean;
-  name: string;
-  company: any;
-  blog: string;
-  location: any;
-  email: any;
-  hireable: any;
-  bio: any;
-  twitter_username: any;
-  public_repos: number;
-  public_gists: number;
-  followers: number;
-  following: number;
-  created_at: string;
-  updated_at: string;
-}
+export type GetUserDataResponse =
+  | any
+  | {
+      login: string;
+      id: number;
+      node_id: string;
+      avatar_url: string;
+      gravatar_id: string;
+      url: string;
+      html_url: string;
+      followers_url: string;
+      following_url: string;
+      gists_url: string;
+      starred_url: string;
+      subscriptions_url: string;
+      organizations_url: string;
+      repos_url: string;
+      events_url: string;
+      received_events_url: string;
+      type: string;
+      site_admin: boolean;
+      name: string;
+      company: any;
+      blog: string;
+      location: any;
+      email: any;
+      hireable: any;
+      bio: any;
+      twitter_username: any;
+      public_repos: number;
+      public_gists: number;
+      followers: number;
+      following: number;
+      created_at: string;
+      updated_at: string;
+    };
 
 export interface OrganisationUser {
   login: string;
@@ -67,4 +71,4 @@ export interface OrganisationUser {
   site_admin: boolean;
 }
 
-export type OrganisationResponse = OrganisationUser[];
+export type GetOrganisationDataResponse = OrganisationUser[] | any;
