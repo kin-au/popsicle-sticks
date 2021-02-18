33f9b38a81b61a286b16c559bdbb07f07e1016f0
kin-au <kinnamau@gmail.com>
2021-02-18 22:52:12 +0000

    add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      sans: [
+        "Noto Sans JP",
+        // "Playfair Display",
+        ...defaultTheme.fontFamily.sans,
+      ],
+    },
   },
   variants: {
     extend: {},
